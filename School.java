6de6f0e8cb13ad1a0277da505c4f47f3fe7d2a84
charu11 <35694779+charu11@users.noreply.github.com>
2018-03-04 07:18:13 +0530

    second little project

---
+++ School.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class School{
+	public static void main(String args[]){
+	Scanner input=new Scanner(System.in);
+	System.out.println("Enter num1:");
+	
+	int num1=input.nextInt();
+	System.out.println("Enter num2");
+	int num2=input.nextInt();
+	
+	int total=num1+num2;
+	
+	System.out.println("Tolal is:"+total);
+	
+	
+
+
+}
+
+
+
+}
